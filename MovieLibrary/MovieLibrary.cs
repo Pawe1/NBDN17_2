28118a277db31c514639633e4b9c8bd2762819d8
Natalia Kowalik <Natalia.Kowalik@ihs.com>
2017-09-19 15:56:11 +0200

    working solution

--- MovieLibrary/MovieLibrary.cs
+++ MovieLibrary/MovieLibrary.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace TrainingPrep.collections
 {
@@ -22,6 +23,35 @@ namespace TrainingPrep.collections
         {
             if (!movies.Contains(movie)) 
                 movies.Add(movie);
+        }
+
+        public IEnumerable<Movie> all_movies_published_by_pixar()
+        {
+            foreach (var movie in movies)
+            {
+                if (movie.production_studio.Equals(ProductionStudio.Pixar))
+                {
+                    yield return movie;  
+                }
+            }
+        }
+
+        public IEnumerable<Movie> sort_all_movies_by_title_descending()
+        {
+            List<Movie> allMovies = all_movies().ToList();
+
+            allMovies.Sort(new MyComparer());
+            var sortedMovies = allMovies;
+
+            return new ReadOnlySet<Movie>(sortedMovies);
+        }
+
+        public class MyComparer : IComparer<Movie>
+        {
+            public int Compare(Movie x, Movie y)
+            {
+                return -(x.title.CompareTo(y.title));
+            }
         }
     }
 }
